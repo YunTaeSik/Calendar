c08ca646a8f75db58fed2f55b9ec0bbd797ad789
YunTaeSik <sky877kr@gmail.com>
2016-10-31 22:23:03 +0900

    인트로 화면 추가 프래그먼트 추가

---
+++ app/src/main/java/com/funnytoday/project/calendar/main/MainActivity.java
@@ -0,0 +1,68 @@
+package com.funnytoday.project.calendar.main;
+
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.funnytoday.project.calendar.R;
+import com.funnytoday.project.calendar.fragment.DayFragment;
+import com.funnytoday.project.calendar.fragment.MouthFragment;
+import com.funnytoday.project.calendar.fragment.WeekFragment;
+
+public class MainActivity extends AppCompatActivity {
+    private FragmentTransaction fragmentTransaction;
+    private ActionBar actionBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initActionBar();
+        initFragment();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_actionbar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        switch (item.getItemId()) {
+            case R.id.monthly_btn:
+                fragmentTransaction.replace(R.id.main_frame, new MouthFragment());
+                fragmentTransaction.commit();
+                actionBar.setTitle("월별보기");
+                break;
+            case R.id.weekly_btn:
+                fragmentTransaction.replace(R.id.main_frame, new WeekFragment());
+                fragmentTransaction.commit();
+                actionBar.setTitle("주별보기");
+                break;
+            case R.id.day_btn:
+                fragmentTransaction.replace(R.id.main_frame, new DayFragment());
+                fragmentTransaction.commit();
+                actionBar.setTitle("일별보기");
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void initActionBar() {
+        actionBar = getSupportActionBar();
+        actionBar.setTitle("월별보기");
+    }
+
+    private void initFragment() {
+        fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.add(R.id.main_frame, new MouthFragment());
+        fragmentTransaction.commit();
+    }
+}
