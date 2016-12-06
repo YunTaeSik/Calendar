12dfb7a2d75c4293371e6127a00f1a87ef70c96f
YunTaeSik <sky877kr@gmail.com>
2016-12-06 22:35:08 +0900

    주별 리사이클뷰 구현(로직 때문에 완벽히 구현불가능)

--- app/src/main/java/com/funnytoday/project/calendar/adapter/WeeklyPagerAdapter.java
+++ app/src/main/java/com/funnytoday/project/calendar/adapter/WeeklyPagerAdapter.java
@@ -2,8 +2,13 @@ package com.funnytoday.project.calendar.adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,9 +17,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.funnytoday.project.calendar.R;
+import com.funnytoday.project.calendar.db.DBManager;
 import com.funnytoday.project.calendar.util.Contact;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 
 /**
@@ -26,6 +33,10 @@ public class WeeklyPagerAdapter extends PagerAdapter {
     private ViewHolder viewHolder;
     private WeeklyBaseAdapter weeklyBaseAdapter;
     private Calendar calendar;
+    private WriteRecyAdapter writeRecyAdapter;
+    private RecyclerView.LayoutManager R_layoutManager;
+    private DBManager dbManager;
+    private SQLiteDatabase redadb;
 
     public WeeklyPagerAdapter(Context context) {
         this.context = context;
@@ -58,6 +69,7 @@ public class WeeklyPagerAdapter extends PagerAdapter {
         viewHolder.calendar_text_week = (TextView) viewpager.findViewById(R.id.calendar_text_week);
         viewHolder.weekly_left_image = (ImageView) viewpager.findViewById(R.id.weekly_left_image);
         viewHolder.weekly_right_image = (ImageView) viewpager.findViewById(R.id.weekly_right_image);
+        viewHolder.write_recycleview = (RecyclerView) viewpager.findViewById(R.id.write_recycleview);
 
         calendar = getCalendar(position);
         viewHolder.calendar_text_year.setText(String.valueOf(calendar.get(Calendar.YEAR)));
@@ -82,6 +94,37 @@ public class WeeklyPagerAdapter extends PagerAdapter {
 
         weeklyBaseAdapter = new WeeklyBaseAdapter(context, calendar);
         viewHolder.weekly_grid.setAdapter(weeklyBaseAdapter);
+
+        dbManager = new DBManager(context, "Write", null, 1);
+        redadb = dbManager.getReadableDatabase();
+        String table_name = viewHolder.calendar_text_year.getText().toString() + viewHolder.calendar_text_mouth.getText().toString()
+                + String.valueOf(calendar.get(Calendar.DATE));
+
+        ArrayList starttime = new ArrayList();
+        ArrayList endtime = new ArrayList();
+        ArrayList title = new ArrayList();
+        ArrayList jsonarray = new ArrayList();
+
+        try {
+            Cursor cursor = redadb.query("'" + table_name + "'", null, null, null, null, null, null);
+            for (int i = 0; i < cursor.getCount(); i++) {
+                cursor.moveToPosition(i);
+                starttime.add(cursor.getString(1));
+                endtime.add(cursor.getString(2));
+                title.add(cursor.getString(3));
+                jsonarray.add(cursor.getString(4));
+            }
+
+            viewHolder.write_recycleview.setVisibility(View.VISIBLE);
+            viewHolder.write_recycleview.setHasFixedSize(true);
+            R_layoutManager = new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL);
+            viewHolder.write_recycleview.setLayoutManager(R_layoutManager);
+            writeRecyAdapter = new WriteRecyAdapter(context, starttime, endtime, title, jsonarray, calendar); //값이 있을때
+            viewHolder.write_recycleview.setAdapter(writeRecyAdapter);
+        } catch (SQLiteException e) {
+            viewHolder.write_recycleview.setVisibility(View.GONE);
+        }
+
         container.addView(viewpager);
         return viewpager;
     }
@@ -100,6 +143,7 @@ public class WeeklyPagerAdapter extends PagerAdapter {
         private TextView calendar_text_year;
         private TextView calendar_text_mouth;
         private TextView calendar_text_week;
+        private RecyclerView write_recycleview;
     }
 
     @Override
