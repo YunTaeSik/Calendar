bf074961d053819631a653c190cd5a58c6fb20be
ChoiWonGyun <chl8263@naver.com>
2016-12-09 13:26:32 +0900

    알람 꾸미기 피드백 ㄱㄱ

--- app/src/main/java/com/funnytoday/project/calendar/alarm/AlarmActivity.java
+++ app/src/main/java/com/funnytoday/project/calendar/alarm/AlarmActivity.java
@@ -2,14 +2,32 @@ package com.funnytoday.project.calendar.alarm;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.funnytoday.project.calendar.R;
 
 public class AlarmActivity extends AppCompatActivity {
-
+    Button btnok;
+    Button btncancel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm);
+        btnok= (Button)findViewById(R.id.ok);
+        btncancel=(Button) findViewById(R.id.cancel);
+        btnok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+        btncancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
